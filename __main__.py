b16701d52116f5ddc7c1e91cdc1c83113091445a
Ren Tatsumoto <tatsu@autistici.org>
2021-08-06 09:33:04 +0300

    update examples

--- __main__.py
+++ __main__.py
@@ -2,15 +2,17 @@ from .mecab_controller import MecabController
 
 mecab = MecabController()
 
-expr = u"カリン、自分でまいた種は自分で刈り取れ"
+expr = "カリン、自分でまいた種は自分で刈り取れ"
 print(mecab.reading(expr))
-expr = u"昨日、林檎を2個買った。"
+expr = "昨日、林檎を2個買った。"
 print(mecab.reading(expr))
-expr = u"真莉、大好きだよん＾＾"
+expr = "真莉、大好きだよん＾＾"
 print(mecab.reading(expr))
-expr = u"彼２０００万も使った。"
+expr = "彼２０００万も使った。"
 print(mecab.reading(expr))
-expr = u"彼二千三百六十円も使った。"
+expr = "彼二千三百六十円も使った。"
 print(mecab.reading(expr))
-expr = u"千葉"
+expr = "千葉"
+print(mecab.reading(expr))
+expr = "昨日すき焼きを食べました"
 print(mecab.reading(expr))
