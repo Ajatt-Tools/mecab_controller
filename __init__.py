be0c1283e4736c1f6584d04d780ff4233f4ab46e
Ren Tatsumoto <tatsu@autistici.org>
2023-06-16 21:01:23 +0300

    add a new import

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from .mecab_controller import BasicMecabController, MecabController
 from .format import format_output
-from .kana_conv import to_hiragana, to_katakana, is_kana_str
+from .kana_conv import to_hiragana, to_katakana, is_kana_str, kana_to_moras
