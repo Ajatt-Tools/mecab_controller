ab13c0f4be80f47bef4b70279f961112333ab8ed
Ren Tatsumoto <tatsu@autistici.org>
2024-02-25 18:03:59 +0300

    add a simple main

--- __main__.py
+++ __main__.py
@@ -1,2 +1,15 @@
 # Copyright: Ren Tatsumoto <tatsu at autistici.org> and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+import sys
+
+from .mecab_controller import MecabController
+
+
+def main():
+    mecab = MecabController(verbose=False)
+    print(mecab.reading(" ".join(sys.argv[1:])))
+
+
+if __name__ == '__main__':
+    main()
