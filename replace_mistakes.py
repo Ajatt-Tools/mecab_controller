c0b2cd002a7e02e2178f0003032ef4fde75f1c6b
Ren Tatsumoto <tatsu@autistici.org>
2024-08-22 20:28:54 +0300

    fix mistakes

--- replace_mistakes.py
+++ replace_mistakes.py
@@ -26,12 +26,71 @@ def replace_mistake(context: Sequence[MecabParsedToken], pos: int) -> Iterable[M
     token = context[pos]
     if token.word == "放っ" and slice_headwords(context, pos + 1, pos + 3) in (("て", "おく"), ("て", "おける")):
         yield dataclasses.replace(token, headword="放る", katakana_reading="ホウッ")
+    elif token.word == "しや" and token.headword == "視野":
+        yield dataclasses.replace(
+            token,
+            headword="してやる",
+            part_of_speech=PartOfSpeech.verb,
+            inflection_type=Inflection.nominal_connection_2,
+        )
+    elif token.word == "いいっ" and token.headword == "いい":
+        yield MecabParsedToken(
+            word="いい",
+            headword="いい",
+            katakana_reading="イイ",
+            part_of_speech=PartOfSpeech.i_adjective,
+            inflection_type=Inflection.dictionary_form,
+        )
+        yield MecabParsedToken(
+            word="っ",
+            headword="っ",
+            katakana_reading="ッ",
+            part_of_speech=PartOfSpeech.unknown,
+            inflection_type=Inflection.unknown,
+        )
+    elif token.word == "本当のところ":
+        yield MecabParsedToken(
+            word="本当",
+            headword="本当",
+            katakana_reading="ホントウ",
+            part_of_speech=PartOfSpeech.noun,
+            inflection_type=Inflection.dictionary_form,
+        )
+        yield MecabParsedToken(
+            word="の",
+            headword="の",
+            katakana_reading="ノ",
+            part_of_speech=PartOfSpeech.particle,
+            inflection_type=Inflection.unknown,
+        )
+        yield MecabParsedToken(
+            word="ところ",
+            headword="ところ",
+            katakana_reading="トコロ",
+            part_of_speech=PartOfSpeech.noun,
+            inflection_type=Inflection.dictionary_form,
+        )
     elif token.word == "有り難う" and token.katakana_reading == "アリガタウ":
         yield dataclasses.replace(token, katakana_reading="アリガトウ")
     elif token.word == "出て" and token.headword == "出し手" and token.katakana_reading == "ダシテ":
         yield dataclasses.replace(token, headword="出る", katakana_reading="デテ")
     elif token.word == "悪い" and token.katakana_reading == "アクイ" and token.headword == "悪意":
         yield dataclasses.replace(token, headword="悪い", katakana_reading="ワルイ")
+    elif token.word == "では" and token.katakana_reading == "デハ" and token.headword == "出端":
+        yield MecabParsedToken(
+            word="で",
+            headword="で",
+            katakana_reading="デ",
+            part_of_speech=PartOfSpeech.particle,
+            inflection_type=Inflection.unknown,
+        )
+        yield MecabParsedToken(
+            word="は",
+            headword="は",
+            katakana_reading="ハ",
+            part_of_speech=PartOfSpeech.particle,
+            inflection_type=Inflection.unknown,
+        )
     elif token.word == "いた目" and token.katakana_reading == "イタメ" and token.headword == "板目":
         yield MecabParsedToken(
             word="い",
