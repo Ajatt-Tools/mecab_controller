75529c5e8d790df172e16ed7225336fab1dc7c30
Ren Tatsumoto <tatsu@autistici.org>
2024-10-04 17:39:14 +0300

    fix mistake

--- replace_mistakes.py
+++ replace_mistakes.py
@@ -41,6 +41,22 @@ def take_headword(context: Sequence[WrappedToken], pos: int) -> Optional[str]:
 def replace_mistake(token: MecabParsedToken, context: Sequence[WrappedToken], pos: int) -> Iterable[MecabParsedToken]:
     if token.word == "放っ" and slice_headwords(context, pos + 1, pos + 3) in (("て", "おく"), ("て", "おける")):
         yield dataclasses.replace(token, headword="放る", katakana_reading="ホウッ")
+    elif token.word == "はおら" and token.headword == "はおる" and "ぬ" == take_headword(context, pos + 1):
+        # Xはおらぬ
+        yield MecabParsedToken(
+            word="は",
+            headword="は",
+            katakana_reading="ハ",
+            part_of_speech=PartOfSpeech.particle,
+            inflection_type=Inflection.unknown,
+        )
+        yield MecabParsedToken(
+            word="おら",
+            headword="おる",
+            katakana_reading="オラ",
+            part_of_speech=PartOfSpeech.verb,
+            inflection_type=Inflection.irrealis,
+        )
     elif token.word == "降り" and token.katakana_reading == "オリ" and "が" == take_headword(context, pos - 1):
         # 雪が降りました: オ=>フ
         yield dataclasses.replace(
